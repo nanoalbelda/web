52250b26dc14210e040a2577e080e0455cab51f8
nanoalbelda <44122700+nanoalbelda@users.noreply.github.com>
2022-06-15 17:11:39 +0200

    Delete stop-process.sh

--- stop-process.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-
-isExistApp=`ps -ef | grep basico-0.0.1-SNAPSHOT.jar`
-if [[ -n  $isExistApp ]]; then
-    ps -ef | grep basico-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | xargs kill
-       
-fi
