bd3e557b3d7fddb05e53a6268e499b05adb35665
nanoalbelda <44122700+nanoalbelda@users.noreply.github.com>
2022-06-15 17:04:14 +0200

    Update start-process.sh

--- start-process.sh
+++ start-process.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -jar /home/ec2-user/artifact_name.jar > /dev/null 2> /dev/null < /dev/null &
+java -jar /home/ec2-user/basico-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
 
