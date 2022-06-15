c63053dc34fc1c1d921fd300e29274a8810201f2
nanoalbelda <44122700+nanoalbelda@users.noreply.github.com>
2022-06-15 16:46:50 +0200

    Delete basico/scripts directory

--- basico/scripts/start-process.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-java -jar /home/ec2-user/artifact_name.jar > /dev/null 2> /dev/null < /dev/null &
-
--- basico/scripts/stop-process.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ps -ef | grep basico-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | xargs kill
